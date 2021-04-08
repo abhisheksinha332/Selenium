58261661272a6500abbe52f3aa6c81defaedc0b9
Abhishek Sinha <3327abhishek@gmail.com>
2021-04-09 00:54:16 +0530

    Maven setup

---
+++ SeleniumThree/src/testCases/TestBrowsers.java
@@ -0,0 +1,15 @@
+package testCases;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestBrowsers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.gecko.driver", "D:\\ABHISHEK\\Udemy\\selenium\\executables\\geckodriver.exe");
+		FirefoxDriver driver = new FirefoxDriver();
+		
+		driver.get("http://abhishek-sinha.netlify.app/");
+	}
+
+}
